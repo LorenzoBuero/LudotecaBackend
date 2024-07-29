3926dc97576f2c258551d8f9e140d69f4c666b04
LorenzoBuero <138243046+LorenzoBuero@users.noreply.github.com>
2024-07-29 18:07:06 -0300

    Arreglado bug en el que permite ingresar el ID al crear una reseña

--- EvDjango/LudotecaDj/resenias/models.py
+++ EvDjango/LudotecaDj/resenias/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)  #id = models.IntegerField(primary_key=True)
     
     titulo = models.CharField(max_length=100, verbose_name = 'título', default="")
     subtitulo_1 = models.CharField(max_length=200, verbose_name = 'subtitulo 1', default="")
